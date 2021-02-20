866df066aa3cca9e1d51363213e5991726376aca
Joseph Vega <jcvb0206@gmail.com>
2021-02-20 11:40:50 -0500

    Set associations in User model and add attached image in models (previous ERD)

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,2 +1,3 @@
 class Faculty < ApplicationRecord
+  has_one_attached :cover
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
   has_many :involved_users, dependent: :destroy
   has_many :users, through: :involved_users
+  has_one_attached :cover
 end
